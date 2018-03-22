dd10ec078a942bbe1044f034d4dfc89ef416f498
Vasyl Motsyak <neformen@gmail.com>
2018-03-22 13:38:55 +0200

    add middleware

--- server/index.ts
+++ server/index.ts
@@ -15,7 +15,7 @@ createConnection().then(async connection => {
     const app = express();
     app.use(bodyParser.json());
     app.use(express.static(path.join(__dirname, './../dist')));
-    app.use(/^\/(?!api).*/, express.static(path.join(__dirname, './../dist')));
+    //app.use(/^\/(?!api).*/, express.static(path.join(__dirname, './../dist')));
 
     // register express routes from defined application routes
     Routes.forEach(route => {
@@ -28,6 +28,11 @@ createConnection().then(async connection => {
                 res.json(result);
             }
         });
+    });
+  
+    app.use(function(err, req, res, next) {
+      console.error(err);
+      next();
     });
 
     // setup express app here
